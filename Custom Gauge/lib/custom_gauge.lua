dfc10ece45fcb60fc5a19746fe79b6ae508df7d1
marcelompaz <marcelopaz@gmail.com>
2023-09-01 23:30:39 +0200

    EMB-110 throttle

--- Custom Gauge/lib/custom_gauge.lua
+++ Custom Gauge/lib/custom_gauge.lua
@@ -1,199 +1,149 @@
-L = 400
-W = L
-R = (L / 2) * 0.95
-Cx = L / 2
-Cy = W / 2
-
-MAJOR_TICK = 16
-MINOR_TICK = 8
-
-TICK_COLOR = "white"
-BACKGROUND = "gray"
-GAUGE_BOTTOM = "black"
-
-INITIAL_ANGLE = 120
-END_ANGLE = 400
-NUM_TICKS = 23
-INTERNAL_TICKS = 3
-
-
-
-
-TICK_LABELS = {"0", "20", "40", "60", "80", "100", "120", "140", "160", "180", "200"}
-
-FONT = "Inconsolata.ttf"
-FONT_COLOR = "white"
-FONT_SIZE = 25
-INTERNAL_TEXT = true
-
-FONT = "font:" .. FONT .. "; size:" .. FONT_SIZE .. "; color: " .. FONT_COLOR .. "; halign:center; valign:center"
-
-img_needle = nil
-
-VALUES = {
-    min = 0,
-    max = 2200
-}
-
-local settings = { { VALUES.min , INITIAL_ANGLE},
-                   { VALUES.max, END_ANGLE },
-}
-
-
-
-
-ARCS = {{
-    color = "red",
-    initial = 150,
-    final = 180,
-    width = 10,
-    internal = true
-}, {
-    color = "yellow",
-    initial = 120,
-    final = 150,
-    width = 10,
-    internal = true
-}, {
-    color = "green",
-    initial = 60,
-    final = 120,
-    width = 10,
-    internal = true
-}, {
-    color = "white",
-    initial = 40,
-    final = 80,
-    width = 5,
-    internal = false
-}}
-
-NEEDLE = {
-    circle_ratio = 0.15,
-    circle_color = "gray",
-    circle_text = "",
-    size_ratio = 0.85,
-    needle_color = "white",
-    needle_text = "",
-    needle_tickness = 12,
-}
-
-function get_angle(value)
-    return interpolate_linear(settings, value, true)
+function get_angle(value, cfg)
+    return interpolate_linear(cfg.interpolate_table, value, true)
 end
 
-function draw_arc(arc)
-    initial = get_angle(arc.initial)
-    final = get_angle(arc.final)
+function draw_arc(arc, cfg)
+    initial = get_angle(arc.initial, cfg)
+    final = get_angle(arc.final, cfg)
     if arc.internal then
-        _arc(Cx, Cy, initial - 90, final - 90, R - arc.width / 2)
+        _arc(cfg.centerX, cfg.centerY, initial - 90, final - 90, cfg.radius - arc.width / 2)
     else
-        _arc(Cx, Cy, initial - 90, final - 90, R + arc.width / 2)
+        _arc(cfg.centerX, cfg.centerY, initial - 90, final - 90, cfg.radius + arc.width / 2)
     end
     _stroke(arc.color, arc.width)
 end
 
-function draw_tick_text(angle, label, tick_size)
-    if INTERNAL_TEXT then
-        offset = (tick_size * 1.2) + FONT_SIZE / 2
+function draw_tick_text(angle, label, tick_size, cfg)
+    if cfg.internal_text then
+        offset = (tick_size * 1.2) + cfg.font_size / 2
     else
-        offset = -FONT_SIZE / 1.5
+        offset = -cfg.font_size / 1.5
     end
     if label ~= nil then
-        begin_x, begin_y = geo_rotate_coordinates(angle, R - offset)
-        _txt(label, FONT, begin_x + Cx, begin_y + Cy)
+        begin_x, begin_y = geo_rotate_coordinates(angle, cfg.radius - offset)
+        font = "font:" .. cfg.font .. "; size:" .. cfg.font_size .. "; color: " .. cfg.font_color ..
+                   "; halign:center; valign:center"
+        _txt(label, font, begin_x + cfg.centerX, begin_y + cfg.centerY)
     end
 end
 
-function draw_line(x1, y1, x2, y2)
-    _move_to(x1, y1)
-    _line_to(x2, y2)
-    _stroke(TICK_COLOR, 3)
-end
-
-function draw_tick(degrees, tick_size)
+function draw_tick(degrees, tick_size, cfg)
     arc_angle = degrees - 90
-    _arc(Cx, Cy, arc_angle-0.5, arc_angle+0.5, R - tick_size / 2) 
-    _stroke(TICK_COLOR, tick_size)  
+    _arc(cfg.centerX, cfg.centerY, arc_angle - 0.5, arc_angle + 0.5, cfg.radius - tick_size / 2)
+    _stroke(cfg.tick_color, tick_size)
 end
 
-function draw_needle(needle)
-    circle_r = R * needle.circle_ratio
-    needle_size = R * needle.size_ratio 
-    
-    _move_to(Cx, Cy)
-    _line_to(Cx, Cy-needle_size)
+function draw_needle(needle, cfg)
+    circle_r = cfg.radius * needle.circle_ratio
+    needle_size = cfg.radius * needle.size_ratio
+
+    _move_to(cfg.centerX, cfg.centerY)
+    _line_to(cfg.centerX, cfg.centerY - needle_size)
     _stroke(needle.needle_color, needle.needle_tickness)
 
-    _triangle(Cx-(needle.needle_tickness/2), Cy-needle_size, Cx+(needle.needle_tickness/2), Cy-needle_size, Cx, Cx - R + MINOR_TICK)
+    _triangle(cfg.centerX - (needle.needle_tickness / 2), cfg.centerY - needle_size,
+        cfg.centerX + (needle.needle_tickness / 2), cfg.centerY - needle_size, cfg.centerX,
+        cfg.centerX - cfg.radius + cfg.minor_tick)
     _fill(needle.needle_color)
 
-    _circle(Cx, Cy, circle_r)
+    _circle(cfg.centerX, cfg.centerY, circle_r)
     _fill(needle.circle_color)
 end
 
+function draw_gauge(cfg)
 
-function draw_gauge()
-    canvas_id = canvas_add(0, 0, L, W)
+    cfg.radius = (cfg.size / 2) * cfg.gauge_ratio
+    cfg.centerX = cfg.size / 2
+    cfg.centerY = cfg.size / 2
+
+    canvas_id = canvas_add(0, 0, cfg.size, cfg.size)
 
     canvas_draw(canvas_id, function()
-        -- Queue a line from 100,100 to 200,200
-        _rect(0, 0, L, W)
-        _fill(BACKGROUND)
-        _circle(Cx, Cy, R)
-        _fill(GAUGE_BOTTOM)
 
-        for _, arc in pairs(ARCS) do
-            draw_arc(arc)
+        _rect(0, 0, cfg.size, cfg.size)
+        _fill(cfg.background)
+        _circle(cfg.centerX, cfg.centerY, cfg.radius)
+        _fill(cfg.gauge_bottom)
+
+        for _, arc in pairs(cfg.arcs) do
+            draw_arc(arc, cfg)
         end
 
-        div = (END_ANGLE - INITIAL_ANGLE) / (NUM_TICKS - 1)
+        div = (cfg.end_angle - cfg.initial_angle) / (cfg.num_ticks - 1)
 
+        -- 
+        for i = 1, cfg.num_ticks do
+            local angle = cfg.initial_angle + (i - 1) * div
+            draw_tick(angle, cfg.major_tick, cfg)
+            draw_tick_text(angle, cfg.tick_labels[i], cfg.major_tick, cfg)
 
-       -- 
-        for i = 1, NUM_TICKS do
-            local angle = INITIAL_ANGLE + (i-1)*div
-            draw_tick(angle, MAJOR_TICK)
-            draw_tick_text(angle, TICK_LABELS[i], MAJOR_TICK)
-
-            internal_div = (div / (INTERNAL_TICKS + 1))
-            if i < NUM_TICKS then
-                for j = 1, INTERNAL_TICKS do
-                    local internal_angle = angle + internal_div + (j-1)*internal_div
-                    draw_tick(internal_angle, MINOR_TICK)
+            internal_div = (div / (cfg.internal_ticks + 1))
+            if i < cfg.num_ticks then
+                for j = 1, cfg.internal_ticks do
+                    local internal_angle = angle + internal_div + (j - 1) * internal_div
+                    draw_tick(internal_angle, cfg.minor_tick, cfg)
                 end
             end
         end
 
     end)
 
-    needle = canvas_add(0, 0, L, W)
+end
+
+LAST_HANDLE = 1
+NEEDLE_VALUES = {}
+
+function add_needle(cfg, needle_cfg)
+    needle = canvas_add(0, 0, cfg.size, cfg.size)
     aaa = canvas_draw(needle, function()
-        
-        draw_needle(NEEDLE)
-        
+
+        draw_needle(needle_cfg, cfg)
+
     end)
+    needle_cfg.handle = LAST_HANDLE
+    table.insert(NEEDLE_VALUES, {
+        current_value = get_angle(cfg.interpolate_table[1][1], cfg),
+        current_needle_value =  get_angle(cfg.interpolate_table[1][1], cfg),
+        needle_canvas = needle,
+        cfg = cfg, 
+        max_needle_movement = needle_cfg.max_movement_per_cycle
+    })
+    LAST_HANDLE = LAST_HANDLE + 1
+    return needle_cfg
 end
 
-CURRENT_VALUE = 0
-CURRENT_NEEDLE_VALUE = 0
-MAX_NEEDLE_MOVEMENT = math.abs((VALUES.min - VALUES.max) * 0.10)
-
-function set_value(value)
-    CURRENT_VALUE = value
+function set_needle_value(needle_cfg, cfg, value)
+    NEEDLE_VALUES[needle_cfg.handle].current_value = get_angle(value, cfg)
 end
+
+-- CURRENT_VALUE = 0
+-- CURRENT_NEEDLE_VALUE = 0
+-- MAX_NEEDLE_MOVEMENT = math.abs((VALUES.min - VALUES.max) * 0.10)
+
+-- function set_value(value)
+--     CURRENT_VALUE = value
+-- end
 
 tmr_update = timer_start(0, 50, function()
 
-    if (math.abs(CURRENT_VALUE - CURRENT_NEEDLE_VALUE) > MAX_NEEDLE_MOVEMENT) then
-        if CURRENT_VALUE > CURRENT_NEEDLE_VALUE then
-            CURRENT_NEEDLE_VALUE = CURRENT_NEEDLE_VALUE + MAX_NEEDLE_MOVEMENT
+    for _, needle in ipairs(NEEDLE_VALUES) do
+        current_needle = needle.current_needle_value
+        current_value = needle.current_value
+
+        if (math.abs(current_value - current_needle) > needle.max_needle_movement) then
+            if current_value > current_needle then
+                current_needle = current_needle + needle.max_needle_movement
+            else
+                current_needle = current_needle - needle.max_needle_movement
+            end
         else
-            CURRENT_NEEDLE_VALUE = CURRENT_NEEDLE_VALUE - MAX_NEEDLE_MOVEMENT
+            current_needle = current_value
         end
-    else
-        CURRENT_NEEDLE_VALUE = CURRENT_VALUE
+
+        needle.current_needle_value = current_needle
+        needle.current_value = current_value
+
+        rotate(needle.needle_canvas, current_needle)
     end
 
-    rotate(needle, get_angle(CURRENT_NEEDLE_VALUE))
 end)
--- Custom Gauge/logic.lua
+++ Custom Gauge/logic.lua
@@ -1,49 +1,75 @@
+local TORQUE_GAUGE_CFG = {
 
-MAJOR_TICK = 16
-MINOR_TICK = 8
+    size = 400,
 
-TICK_COLOR = "white"
-BACKGROUND = "gray"
-GAUGE_BOTTOM = "black"
+    major_tick = 16,
+    minor_tick = 8,
 
-INITIAL_ANGLE = 120
-END_ANGLE = 400
-NUM_TICKS = 23
-INTERNAL_TICKS = 3
+    TICK_COLOR = "white",
+    background = "gray",
+    gauge_bottom = "black",
 
-VALUES = {
-    min = 0,
-    max = 2200
+    initial_angle = 120,
+    end_angle = 400,
+    num_ticks = 23,
+    internal_ticks = 3,
+
+    gauge_ratio = 0.95,
+
+    tick_labels = {"0", "", "2", "", "4", "", "6", "", "8", "", "10", "", "12", "", "14", "", "16", "", "18", "", "20",
+                   "", "22"},
+
+    tick_color = "white",
+
+    arcs = {{
+        color = "green",
+        initial = 400,
+        final = 1970,
+        width = 10,
+        internal = true
+    }, {
+        color = "red",
+        initial = 1970,
+        final = 2000,
+        width = 10,
+        internal = true
+    }},
+
+    font = "Inconsolata.ttf",
+    font_color = "white",
+    font_size = 25,
+    internal_text = true,
+
+    interpolate_table = {{0, 120}, {2200, 400}}
+}
+
+local NEEDLE = {
+    circle_ratio = 0.15,
+    circle_color = "gray",
+    circle_text = "",
+    size_ratio = 0.85,
+    needle_color = "white",
+    needle_text = "",
+    needle_tickness = 12,
+    max_movement_per_cycle = 1.5
 }
 
 
-ARCS = {{
-    color = "green",
-    initial = 400,
-    final = 1970,
-    width = 10,
-    internal = true
-},{
-    color = "red",
-    initial = 1970,
-    final = 2000,
-    width = 10,
-    internal = true
-}}
+rpm_prop = user_prop_add_integer("Engine Number", 1, 4, 1, "Number of the engine that the rpm will be used")
 
-TICK_LABELS = {"0", "","2", "","4", "","6", "","8", "","10", "","12", "","14", "","16", "","18", "","20", "","22"}
+draw_gauge(TORQUE_GAUGE_CFG)
 
-draw_gauge()
+torque_txt = txt_add("TORQUE", "font:Inconsolata-Bold.ttf; size:40; color: white; halign:center; valign:center;", 140,
+    50, 120, 120)
+lbfr_txt = txt_add("LB.FT. x 100", "font:Inconsolata-Bold.ttf; size:25; color: white; halign:center; valign:center;",
+    110, 225, 200, 120)
 
-torque_txt = txt_add("TORQUE", "font:Inconsolata-Bold.ttf; size:40; color: white; halign:center; valign:center;", 140, 50, 120, 120)
-lbfr_txt = txt_add("LB.FT. x 100", "font:Inconsolata-Bold.ttf; size:25; color: white; halign:center; valign:center;", 110, 225, 200, 120)
+torque_needle = add_needle(TORQUE_GAUGE_CFG, NEEDLE)
 
 function new_data_fsx(bus_volts, trq)
-    set_value(trq)
- end
+    set_needle_value(torque_needle, TORQUE_GAUGE_CFG, trq)
+end
 
-fs2020_variable_subscribe("ELECTRICAL MAIN BUS VOLTAGE", "VOLTS",
-                          "ENG TORQUE:1", "Foot pounds", new_data_fsx)
-
-
+fs2020_variable_subscribe("ELECTRICAL MAIN BUS VOLTAGE", "VOLTS", "ENG TORQUE:" .. tostring(user_prop_get(rpm_prop)),
+    "Foot pounds", new_data_fsx)
 
